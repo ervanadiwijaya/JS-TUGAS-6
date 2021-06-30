c94ef7c988bfda34c28db3d4772478b2a5941e19
jauhhari <ervanadiwijaya@gmail.com>
2021-06-30 15:33:40 +0700

    OK

---
+++ tugas6.js
@@ -0,0 +1,16 @@
+
+//make function
+function newFunction() {
+    //make loop to change even number with text "even"
+    for (i = 1; i <= 20; i++) {
+        if (i % 2 == 0) {
+            console.log("genap");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+//call function
+newFunction();
